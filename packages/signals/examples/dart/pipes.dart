1513babe08ca9e9bdfc605924acc180488cbe1c1
Robert Felker <felker.robert@gmail.com>
2023-11-27 21:41:11 +0100

    pipe refill

---
+++ packages/signals/examples/dart/pipes.dart
@@ -0,0 +1,51 @@
+import 'dart:math';
+
+import 'package:signals/signals.dart';
+
+void main() {
+  /*
+  Signal<bool> from a periodic Stream
+   */
+  final timer =
+      Stream.periodic(const Duration(milliseconds: 2000), (ms) {}).toSignal();
+
+  const poolSize = 5;
+
+  final candidat = signal(0);
+
+  /*
+  Signal<int> from an Iterable
+   */
+  final source = List.generate(poolSize, (index) => generate()).toSignal();
+
+  /// Each time the timer move on we take the first element of the list
+  effect(() {
+    // Subscribe to timer event
+    timer.value;
+
+    // We use peak to avoid subscription to source
+    final [head, ...tail] = source.peek();
+
+    // Batch multiple Signals at once.
+    batch(() {
+      source.value = tail;
+      candidat.value = head;
+    });
+  });
+
+  effect(() => print("Qualified to ${candidat.value}"));
+
+  /// Effect on source => refill
+  effect(() {
+    // Refill our source if required
+    while (source.value.length < poolSize) {
+      final complement = generate();
+      source.value = source.value..add(complement);
+      print("Complemented with $complement: ${source.value}\n");
+    }
+  });
+}
+
+final rdm = Random();
+
+int generate() => rdm.nextInt(9999);
