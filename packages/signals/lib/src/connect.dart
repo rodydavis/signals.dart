1d5c9e1e2242817962b06095061914c24af2420c
Robert Felker <felker.robert@gmail.com>
2023-12-01 23:05:59 +0100

    Rename to => from

--- packages/signals/lib/src/connect.dart
+++ packages/signals/lib/src/connect.dart
@@ -2,13 +2,13 @@ import 'dart:async';
 
 import 'signals.dart';
 
-/// Connects a [MutableSignal] to a [Stream].
+/// Connects a [Stream] to a [MutableSignal].
 class Connect<T> {
   Connect(this.signal);
   final MutableSignal<T> signal;
   final Map<int, StreamSubscription> _subscriptions = {};
 
-  /// Connects a [MutableSignal] to a [Stream].
+  /// Connects a [Stream] to a [MutableSignal].
   ///
   /// ```dart
   /// final counter = signal(0);
@@ -17,24 +17,24 @@ class Connect<T> {
   /// final s1 = Stream.value(1);
   /// final s2 = Stream.value(2);
   ///
-  /// c.to(s1).to(s2);
+  /// c.from(s1).from(s2);
   ///
   /// c.dispose();
   /// ```
-  Connect<T> to(
-    Stream<T> stream, {
+  Connect<T> from(
+    Stream<T> source, {
     bool? cancelOnError,
   }) {
-    final subscription = stream.listen(
+    final subscription = source.listen(
       (event) {
         signal.value = event;
       },
       onDone: () {
-        _subscriptions.removeWhere((key, value) => key == stream.hashCode);
+        _subscriptions.removeWhere((key, value) => key == source.hashCode);
       },
       cancelOnError: cancelOnError,
     );
-    _subscriptions[stream.hashCode] = subscription;
+    _subscriptions[source.hashCode] = subscription;
     return this;
   }
 
