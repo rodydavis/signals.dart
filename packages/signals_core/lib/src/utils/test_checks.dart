64bd5f74bab9ae8b8d1ed06e07d149000cb099a3
Robert Felker <felker.robert@gmail.com>
2023-12-28 20:10:42 +0100

    peek

--- packages/signals_core/lib/src/utils/test_checks.dart
+++ packages/signals_core/lib/src/utils/test_checks.dart
@@ -5,10 +5,10 @@ import '../../signals_core.dart';
 /// Extension on 'checks' test lib
 extension SignalChecks<T> on Subject<ReadonlySignal<T>?> {
   /// Compare Signal<T>.peek() to ref T
-  Subject<T> peeked(T ref) {
-    return context.nest<T>(() => ['is not null'], (actual) {
-      if (actual == null) return Extracted.rejection();
-      return Extracted.value(actual.peek());
-    }, atSameLevel: true);
+  void peek(T other) {
+    context.expect(() => prefixFirst('equals ', literal(other)), (actual) {
+      if (actual?.peek() == other) return null;
+      return Rejection(which: ['are not equal']);
+    });
   }
 }
