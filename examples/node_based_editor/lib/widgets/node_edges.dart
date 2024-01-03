d17b437cd275c054585283155f6d6d80df705878
Rody Davis <31253215+rodydavis@users.noreply.github.com>
2024-01-03 14:54:29 -0800

    Update node_edges.dart

--- examples/node_based_editor/lib/widgets/node_edges.dart
+++ examples/node_based_editor/lib/widgets/node_edges.dart
@@ -3,31 +3,28 @@ import 'package:flutter/material.dart';
 import '../nodes/base.dart';
 
 class NodeEdges extends CustomPainter {
+  final Matrix4 matrix4;
   final Map<Node, Offset> nodes;
   final Set<Node> selected;
-  NodeEdges(this.nodes, this.selected);
+  NodeEdges(this.matrix4, this.nodes, this.selected);
 
   @override
   void paint(Canvas canvas, Size size) {
+    canvas.transform(matrix4.storage);
     for (final nodeEntry in nodes.entries) {
-      if (nodeEntry.key.inputs.isEmpty) continue;
-      final source = nodeEntry.value;
-      final sourceSize = nodeEntry.key.size();
-      final sourceOffset = source.translate(
-        sourceSize.width / 2,
-        sourceSize.height / 2,
-      );
-      final paint = Paint()
-        ..color = selected.contains(nodeEntry.key) ? Colors.blue : Colors.grey
-        ..strokeWidth = 1;
-      for (final input in nodeEntry.key.inputs) {
-        final target = nodes[input]!;
-        final targetSize = input.size();
-        final targetOffset = target.translate(
-          targetSize.width / 2,
-          targetSize.height / 2,
-        );
-        canvas.drawLine(sourceOffset, targetOffset, paint);
+      final sourceOffset = nodeEntry.value;
+      if (nodeEntry.key.inputs.isNotEmpty) {
+        final paint = Paint()
+          ..color = selected.contains(nodeEntry.key) ? Colors.blue : Colors.grey
+          ..strokeWidth = 1;
+        final offsets = nodeEntry.key.inputOffsets;
+        for (var i = 0; i < nodeEntry.key.inputs.length; i++) {
+          final target = nodeEntry.key.inputs[i];
+          final nodeOffset =
+              nodes[target]! + target.outputOffset + circleCenter;
+          final targetOffset = sourceOffset + offsets[i] + circleCenter;
+          canvas.drawLine(nodeOffset, targetOffset, paint);
+        }
       }
     }
   }
