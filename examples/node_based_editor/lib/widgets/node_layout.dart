273a866d78b12b77daf32db8f94e28d1094a4550
Rody Davis <rody.davis.jr@gmail.com>
2024-01-03 22:48:49 -0800

    Update node_layout.dart

--- examples/node_based_editor/lib/widgets/node_layout.dart
+++ examples/node_based_editor/lib/widgets/node_layout.dart
@@ -3,15 +3,17 @@ import 'package:flutter/material.dart';
 import '../nodes/base.dart';
 
 class NodeLayout extends MultiChildLayoutDelegate {
+  final Matrix4 matrix;
   final Map<Node, Offset> nodes;
-  NodeLayout(this.nodes);
+  NodeLayout(this.matrix, this.nodes);
 
   @override
   void performLayout(Size size) {
     for (final nodeEntry in nodes.entries) {
-      final nodeSize = BoxConstraints.tight(nodeEntry.key.size());
-      layoutChild(nodeEntry.key.id, nodeSize);
-      positionChild(nodeEntry.key.id, nodeEntry.value);
+      final nodeRect = nodeEntry.value &  nodeEntry.key.size();
+      final transformedRect = MatrixUtils.transformRect(matrix, nodeRect);
+      layoutChild(nodeEntry.key.id, BoxConstraints.tight(transformedRect.size));
+      positionChild(nodeEntry.key.id, transformedRect.topLeft);
     }
   }
 
