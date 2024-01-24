a2af896000671abe73475bac325668f1cfd7cd1f
Rody Davis <rody.davis.jr@gmail.com>
2024-01-24 14:10:08 -0800

    move to extended

---
+++ packages/signals_flutter/lib/extended.dart
@@ -0,0 +1,11 @@
+library flutter_extensions;
+
+// Flutter Signals
+export 'src/signals/flutter/widgets.dart';
+export 'src/signals/flutter/material.dart';
+export 'src/signals/flutter/widgets_binding.dart';
+export 'src/signals/flutter/scheduler_binding.dart';
+
+// Core Signals
+export 'src/signals/core/signal.dart';
+export 'src/signals/core/computed.dart';
