3738bedadae4d0d02430396c58f6616535f9946e
jinyus <starboymc4@gmail.com>
2024-01-24 10:04:36 -0500

    Remove unnecessary ignore: close_sinks comment in toStream() method

--- packages/signals_core/lib/src/extensions/signal.dart
+++ packages/signals_core/lib/src/extensions/signal.dart
@@ -7,7 +7,6 @@ extension ReadonlySignalUtils<T> on ReadonlySignal<T> {
   /// Convert a signal to a [Stream] to be consumed as
   /// a read only stream and also be used in a [StreamBuilder]
   Stream<T> toStream() {
-    // ignore: close_sinks
     final controller = StreamController<T>();
 
     controller.add(value);
