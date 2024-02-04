c310b904520d023930d476fee614ac1549450402
Rody Davis <rody.davis.jr@gmail.com>
2024-02-04 14:05:04 -0800

    remove useSignal/useComputed for now
    Moving to draft PR

--- packages/signals_flutter/lib/extended.dart
+++ packages/signals_flutter/lib/extended.dart
@@ -5,7 +5,3 @@ export 'src/signals/flutter/widgets.dart';
 export 'src/signals/flutter/material.dart';
 export 'src/signals/flutter/widgets_binding.dart';
 export 'src/signals/flutter/scheduler_binding.dart';
-
-// Core Signals
-export 'src/signals/core/signal.dart';
-export 'src/signals/core/computed.dart';
