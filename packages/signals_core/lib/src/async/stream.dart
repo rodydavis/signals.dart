1991cd6cff560cb7641687d7b9628b4f5e8ceff3
Rody Davis <rody.davis.jr@gmail.com>
2024-02-28 23:39:22 -0700

    updating docs

--- packages/signals_core/lib/src/async/stream.dart
+++ packages/signals_core/lib/src/async/stream.dart
@@ -4,8 +4,114 @@ import '../core/signals.dart';
 import 'signal.dart';
 import 'state.dart';
 
-/// A [Signal] that stores value in [AsyncState] and
-/// fetches data from a [Stream]
+/// {@template stream}
+/// Stream signals can be created by extension or method.
+/// 
+/// ### streamSignal
+/// 
+/// ```dart
+/// final stream = () async* {
+///     yield 1;
+/// };
+/// final s = streamSignal(() => stream);
+/// ```
+/// 
+/// ### toSignal()
+/// 
+/// ```dart
+/// final stream = () async* {
+///     yield 1;
+/// };
+/// final s = stream.toSignal();
+/// ```
+/// 
+/// ## .value, .peek()
+/// 
+/// Returns [`AsyncState<T>`](/dart/async/state) for the value and can handle the various states.
+/// 
+/// The `value` getter returns the value of the stream if it completed successfully.
+/// 
+/// > .peek() can also be used to not subscribe in an effect
+/// 
+/// ```dart
+/// final stream = (int value) async* {
+///     yield value;
+/// };
+/// final s = streamSignal(() => stream);
+/// final value = s.value.value; // 1 or null
+/// ```
+/// 
+/// ## .reset()
+/// 
+/// The `reset` method resets the stream to its initial state to recall on the next evaluation.
+/// 
+/// ```dart
+/// final stream = (int value) async* {
+///     yield value;
+/// };
+/// final s = streamSignal(() => stream);
+/// s.reset();
+/// ```
+/// 
+/// ## .refresh()
+/// 
+/// Refresh the stream value by setting `isLoading` to true, but maintain the current state (AsyncData, AsyncLoading, AsyncError).
+/// 
+/// ```dart
+/// final stream = (int value) async* {
+///     yield value;
+/// };
+/// final s = streamSignal(() => stream);
+/// s.refresh();
+/// print(s.value.isLoading); // true
+/// ```
+/// 
+/// ## .reload()
+/// 
+/// Reload the stream value by setting the state to `AsyncLoading` and pass in the value or error as data.
+/// 
+/// ```dart
+/// final stream = (int value) async* {
+///     yield value;
+/// };
+/// final s = streamSignal(() => stream);
+/// s.reload();
+/// print(s.value is AsyncLoading); // true
+/// ```
+/// 
+/// ## Dependencies
+/// 
+/// By default the callback will be called once and the stream will be cached unless a signal is read in the callback.
+/// 
+/// ```dart
+/// final count = signal(0);
+/// final s = streamSignal(() async* {
+///     final value = count();
+///     yield value;
+/// });
+/// 
+/// await s.future; // 0
+/// count.value = 1;
+/// await s.future; // 1
+/// ```
+/// 
+/// If there are signals that need to be tracked across an async gap then use the `dependencies` when creating the `streamSignal` to [`reset`](#.reset()) every time any signal in the dependency array changes.
+/// 
+/// ```dart
+/// final count = signal(0);
+/// final s = streamSignal(
+///     () async* {
+///         final value = count();
+///         yield value;
+///     },
+///     dependencies: [count],
+/// );
+/// s.value; // state with count 0
+/// count.value = 1; // resets the future
+/// s.value; // state with count 1
+/// ```
+/// @link https://dartsignals.dev/dart/async/stream
+/// {@endtemplate}
 class StreamSignal<T> extends AsyncSignal<T> {
   late final Computed<Stream<T>> _stream;
   bool _fetching = false;
@@ -29,8 +135,114 @@ class StreamSignal<T> extends AsyncSignal<T> {
   /// Last value of the stream
   Future<T> get first => _stream.value.first;
 
-  /// A [Signal] that stores value in [AsyncState] and
-  /// fetches data from a [Stream]
+  /// {@template stream}
+  /// Stream signals can be created by extension or method.
+  /// 
+  /// ### streamSignal
+  /// 
+  /// ```dart
+  /// final stream = () async* {
+  ///     yield 1;
+  /// };
+  /// final s = streamSignal(() => stream);
+  /// ```
+  /// 
+  /// ### toSignal()
+  /// 
+  /// ```dart
+  /// final stream = () async* {
+  ///     yield 1;
+  /// };
+  /// final s = stream.toSignal();
+  /// ```
+  /// 
+  /// ## .value, .peek()
+  /// 
+  /// Returns [`AsyncState<T>`](/dart/async/state) for the value and can handle the various states.
+  /// 
+  /// The `value` getter returns the value of the stream if it completed successfully.
+  /// 
+  /// > .peek() can also be used to not subscribe in an effect
+  /// 
+  /// ```dart
+  /// final stream = (int value) async* {
+  ///     yield value;
+  /// };
+  /// final s = streamSignal(() => stream);
+  /// final value = s.value.value; // 1 or null
+  /// ```
+  /// 
+  /// ## .reset()
+  /// 
+  /// The `reset` method resets the stream to its initial state to recall on the next evaluation.
+  /// 
+  /// ```dart
+  /// final stream = (int value) async* {
+  ///     yield value;
+  /// };
+  /// final s = streamSignal(() => stream);
+  /// s.reset();
+  /// ```
+  /// 
+  /// ## .refresh()
+  /// 
+  /// Refresh the stream value by setting `isLoading` to true, but maintain the current state (AsyncData, AsyncLoading, AsyncError).
+  /// 
+  /// ```dart
+  /// final stream = (int value) async* {
+  ///     yield value;
+  /// };
+  /// final s = streamSignal(() => stream);
+  /// s.refresh();
+  /// print(s.value.isLoading); // true
+  /// ```
+  /// 
+  /// ## .reload()
+  /// 
+  /// Reload the stream value by setting the state to `AsyncLoading` and pass in the value or error as data.
+  /// 
+  /// ```dart
+  /// final stream = (int value) async* {
+  ///     yield value;
+  /// };
+  /// final s = streamSignal(() => stream);
+  /// s.reload();
+  /// print(s.value is AsyncLoading); // true
+  /// ```
+  /// 
+  /// ## Dependencies
+  /// 
+  /// By default the callback will be called once and the stream will be cached unless a signal is read in the callback.
+  /// 
+  /// ```dart
+  /// final count = signal(0);
+  /// final s = streamSignal(() async* {
+  ///     final value = count();
+  ///     yield value;
+  /// });
+  /// 
+  /// await s.future; // 0
+  /// count.value = 1;
+  /// await s.future; // 1
+  /// ```
+  /// 
+  /// If there are signals that need to be tracked across an async gap then use the `dependencies` when creating the `streamSignal` to [`reset`](#.reset()) every time any signal in the dependency array changes.
+  /// 
+  /// ```dart
+  /// final count = signal(0);
+  /// final s = streamSignal(
+  ///     () async* {
+  ///         final value = count();
+  ///         yield value;
+  ///     },
+  ///     dependencies: [count],
+  /// );
+  /// s.value; // state with count 0
+  /// count.value = 1; // resets the future
+  /// s.value; // state with count 1
+  /// ```
+  /// @link https://dartsignals.dev/dart/async/stream
+  /// {@endtemplate}
   StreamSignal(
     Stream<T> Function() callback, {
     this.cancelOnError,
--- packages/signals_core/lib/src/core/observer.dart
+++ packages/signals_core/lib/src/core/observer.dart
@@ -1,6 +1,32 @@
 import 'signals.dart';
 
-/// A class that listens to signals and computed changes.
+/// {@template observer}
+/// You can observe all signal values in the dart application by providing an implementation of `SignalsObserver`:
+/// 
+/// ```dart
+/// abstract class SignalsObserver {
+///   void onSignalCreated(Signal instance);
+///   void onSignalUpdated(Signal instance, dynamic value);
+///   void onComputedCreated(Computed instance);
+///   void onComputedUpdated(Computed instance, dynamic value);
+///   static SignalsObserver? instance;
+/// }
+/// ```
+/// 
+/// > There is a prebuilt `LoggingSignalsObserver` for printing updates to the console.
+/// 
+/// To add the observer override the instance at the start of the application:
+/// 
+/// ```dart
+/// void main() {
+///     SignalsObserver.instance = LoggingSignalsObserver(); // or custom observer
+///     ...
+/// }
+/// ```
+/// 
+/// This will have a slight performance hit since every update will be tracked via the observer. It is recommended to only set the `SignalsObserver.instance` in debug or profile mode.
+/// @link https://dartsignals.dev/dart/utilities/observer
+/// {@endtemplate}
 abstract class SignalsObserver {
   /// Called when a signal is created.
   void onSignalCreated(Signal instance);
