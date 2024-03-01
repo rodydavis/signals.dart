8bdb517e4a37a37e5c716b4392046eab9857ffd8
Rody Davis <rody.davis.jr@gmail.com>
2024-02-29 18:07:56 -0700

    Fixing #204 by not disposing effect on hot reload

--- packages/signals_flutter/lib/src/watch/widget.dart
+++ packages/signals_flutter/lib/src/watch/widget.dart
@@ -3,9 +3,9 @@ import 'package:signals_core/signals_core.dart';
 
 /// {@template watch}
 /// ## Watch
-/// 
+///
 /// To watch a signal for changes in Flutter, use the `Watch` widget. This will only rebuild this widget method and not the entire widget tree.
-/// 
+///
 /// ```dart
 /// final signal = signal(10);
 /// ...
@@ -14,13 +14,13 @@ import 'package:signals_core/signals_core.dart';
 ///   return Watch((context) => Text('$signal'));
 /// }
 /// ```
-/// 
+///
 /// This will also automatically unsubscribe when the widget is disposed.
-/// 
+///
 /// Any inherited widgets referenced to inside the Watch scope will be subscribed to for updates ([MediaQuery](https://api.flutter.dev/flutter/widgets/MediaQuery-class.html), [Theme](https://api.flutter.dev/flutter/material/Theme-class.html), etc.) and retrigger the builder method.
-/// 
+///
 /// There is also a drop in replacement for builder:
-/// 
+///
 /// ```diff
 /// final signal = signal(10);
 /// ...
@@ -32,12 +32,12 @@ import 'package:signals_core/signals_core.dart';
 ///   );
 /// }
 /// ```
-/// 
-/// 
+///
+///
 /// ## .watch(context)
-/// 
+///
 /// If you need to map to a widget property use the `watch` extension method. This will infer the type and subscribe to the signal.
-/// 
+///
 /// ```dart
 /// final fontSize = signal(10);
 /// ...
@@ -48,13 +48,13 @@ import 'package:signals_core/signals_core.dart';
 ///   );
 /// }
 /// ```
-/// 
+///
 /// It is recommended to use `Watch` instead of `.watch(context)` as it will automatically unsubscribe when the widget is disposed instead of waiting on the garbage collector via [WeakReferences](https://api.flutter.dev/flutter/dart-core/WeakReference-class.html).
-/// 
+///
 /// ## .listen(context, cb)
-/// 
+///
 /// Alternatively if need to listen for changes to a signal but not rebuild the widget you can use the listen extension.
-/// 
+///
 /// ```dart
 /// final counter = signal(0);
 /// ...
@@ -72,17 +72,17 @@ import 'package:signals_core/signals_core.dart';
 /// ...
 /// }
 /// ```
-/// 
+///
 /// This can be used in the build method and will call the callback method in the same way it would rebuild the widget (only when mounted).
-/// 
+///
 /// ### Rebuilds
-/// 
+///
 /// To protect against unnecessary rebuilds, the `watch` extension will only subscribe once to the nearest element and mark the widget as dirty.
-/// 
+///
 /// This means that if you have multiple widgets that are watching the same signal, only the first one will be subscribed to the signal and multiple updates will be batched together.
-/// 
+///
 /// It is also possible to isolate the rebuilds with the `Builder` widget, however it is recommended to use `Watch` or `SignalWidget` instead.
-/// 
+///
 /// ```dart
 /// final signal = signal(10);
 /// ...
@@ -103,11 +103,11 @@ import 'package:signals_core/signals_core.dart';
 ///   );
 /// }
 /// ```
-/// 
+///
 /// ## Selectors
-/// 
+///
 /// With signals instead of using `select` you instead create a new `computed` signal that is derived from the original signal.
-/// 
+///
 /// ```dart
 /// final signal = signal((a: 1, b: 2));
 /// final computed = computed(() => signal.value.a);
@@ -117,9 +117,9 @@ import 'package:signals_core/signals_core.dart';
 ///   return Watch((_) => Text('$computed'));
 /// }
 /// ```
-/// 
+///
 /// It is also possible to select from the signal directly:
-/// 
+///
 /// ```dart
 /// final signal = signal((a: 1, b: 2));
 /// final computed = signal.select((s) => s.value.a);
@@ -153,8 +153,6 @@ class _WatchState<T extends Widget> extends State<Watch<T>> {
     super.reassemble();
     reloadSignalsDevTools();
     child = null;
-    fn?.call();
-    fn = null;
     rebuild();
   }
 
