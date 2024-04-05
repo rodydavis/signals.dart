715d6de9745af18ac86b9dd4ffede0cf51ffd267
Rody Davis <rody.davis.jr@gmail.com>
2024-04-06 00:49:03 +1100

    adding more tests

---
+++ packages/signals_core/test/core/untracked_test.dart
@@ -0,0 +1,32 @@
+import 'package:signals_core/signals_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('untracked', () {
+    test('single', () {
+      final s = signal(0);
+      int calls = 0;
+      final dis = effect(() {
+        s.value;
+        untracked(() => s.value + 1);
+        calls++;
+      });
+      dis();
+
+      expect(calls, 1);
+    });
+
+    test('nested', () {
+      final s = signal(0);
+      int calls = 0;
+      final dis = effect(() {
+        s.value;
+        untracked(() => untracked(() => s.value + 1));
+        calls++;
+      });
+      dis();
+
+      expect(calls, 1);
+    });
+  });
+}
