ecd5739da8693d6784e83642cea7bcd3cf293f62
Rody Davis <rody.davis.jr@gmail.com>
2024-04-07 22:48:27 +1000

    updating web example

--- examples/html_todo_app/web/main.dart
+++ examples/html_todo_app/web/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:js_interop';
 import 'package:web/helpers.dart';
 import 'package:signals/signals.dart';
@@ -5,7 +6,7 @@ import 'package:signals/signals.dart';
 void main() {
   final todoForm = document.getElementById("todoForm") as HTMLFormElement;
   final todoInput = document.getElementById("todoInput") as HTMLInputElement;
-  final todoList = document.getElementById("todoList") as HTMLLIElement;
+  final todoList = document.getElementById("todoList") as HTMLTableElement;
   final taskFilter = document.getElementById("taskFilter") as HTMLInputElement;
 
   final app = TasksApp();
@@ -14,8 +15,9 @@ void main() {
     event.preventDefault();
     final title = todoInput.value.trim();
     if (title.isEmpty) return;
-    final task = TaskElement.create((title: title, completed: false));
+    final task = TaskElement.create(app, (title: title, completed: false));
     app.tasks.add(task);
+    app.save();
     todoInput.value = "";
   }.toJS;
 
@@ -47,9 +49,20 @@ void main() {
     final el = document.getElementById("tasks-completed")!;
     el.innerHTML = app.completedTaskCount.value.toString();
   });
+
+  app.load();
 }
 
 typedef Task = ({bool completed, String title});
+
+extension on Task {
+  Map<String, Object> toJson() {
+    return {
+      'title': title,
+      'completed': completed,
+    };
+  }
+}
 
 class TasksApp {
   final tasks = listSignal<TaskElement>([]);
@@ -78,42 +91,107 @@ class TasksApp {
     // thus, we can avoid an unnecessary subscription
     return taskCount.peek() - activeTaskCount.value;
   });
+
+  void save() {
+    window.localStorage.setItem(
+      'tasks',
+      jsonEncode(tasks.map((e) => e.state.value.toJson()).toList()),
+    );
+  }
+
+  void load() {
+    final saved = window.localStorage.getItem('tasks');
+    if (saved != null && saved.isNotEmpty) {
+      final items = (jsonDecode(saved) as List).cast<Map>();
+      batch(() {
+        for (final item in items) {
+          final task = TaskElement.create(this, (
+            completed: item['completed'] as bool,
+            title: item['title'] as String,
+          ));
+          tasks.add(task);
+        }
+      });
+    }
+  }
 }
 
 class TaskElement {
   final Element root;
-  final Function cleanup;
+  final List<Function> cleanup;
   final Signal<Task> state;
+  final TasksApp app;
 
-  TaskElement(this.state, this.root, this.cleanup);
+  TaskElement(this.app, this.state, this.root, this.cleanup);
 
-  static TaskElement create(Task task) {
-    final listItem = document.createElement("li");
-    final label = document.createElement("label");
-    final checkbox = document.createElement("input") as HTMLInputElement;
-    checkbox.type = "checkbox";
-    checkbox.checked = task.completed;
+  static TaskElement create(TasksApp app, Task task) {
+    final base = document.createElement("tr");
     final state = signal(task);
-    final cleanup = effect(() {
-      final change = (Event e) {
-        state.value = (
-          title: state.value.title,
-          completed: checkbox.checked,
-        );
+    final cleanup = <Function>[];
+    final instance = TaskElement(app, state, base, cleanup);
+    base.appendChild(() {
+      final select = state.select((e) => e.value.title);
+      final el = document.createElement("td");
+      final ctl = document.createElement("input") as HTMLInputElement;
+      ctl.type = 'text';
+      ctl.value = select.value;
+      cleanup.add(effect(() {
+        final change = (Event e) {
+          state.value = (
+            title: ctl.value,
+            completed: state.value.completed,
+          );
+          app.save();
+        }.toJS;
+        ctl.addEventListener("change", change);
+        return () {
+          ctl.removeEventListener("change", change);
+        };
+      }));
+      el.appendChild(ctl);
+      return el;
+    }());
+    base.appendChild(() {
+      final select = state.select((e) => e.value.completed);
+      final el = document.createElement("td");
+      final ctl = document.createElement("input") as HTMLInputElement;
+      ctl.type = 'checkbox';
+      ctl.checked = select.value;
+      cleanup.add(effect(() {
+        final change = (Event e) {
+          state.value = (
+            title: state.value.title,
+            completed: ctl.checked,
+          );
+          app.save();
+        }.toJS;
+        ctl.addEventListener("change", change);
+        return () {
+          ctl.removeEventListener("change", change);
+        };
+      }));
+      el.appendChild(ctl);
+      return el;
+    }());
+    base.appendChild(() {
+      final el = document.createElement("td");
+      final btn = document.createElement("button") as HTMLButtonElement;
+      btn.innerHTML = 'delete';
+      btn.onclick = (Event e) {
+        instance.delete();
       }.toJS;
-      checkbox.addEventListener("change", change);
-      return () {
-        checkbox.removeEventListener("change", change);
-      };
-    });
-    label.appendChild(checkbox);
-    label.append(" ${task.title}".toJS);
-    listItem.appendChild(label);
-    return TaskElement(state, listItem, cleanup);
+      el.appendChild(btn);
+      return el;
+    }());
+    return instance;
   }
 
-  void remove() {
-    cleanup();
+  void delete() {
+    for (final cb in cleanup) {
+      cb();
+    }
     root.remove();
+    app.tasks.removeWhere((e) => e.root == root);
+    app.save();
   }
 }
