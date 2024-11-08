e385a05233ab297a7c3808f906a4f36a65f6dc0a
Rody Davis <31253215+rodydavis@users.noreply.github.com>
2024-11-07 21:48:07 -0300

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -8,7 +8,9 @@
     ];
     bootstrap = ''
         mkdir "$out"
-        cp -a -rf ${./examples}/${example}/. "$out/"
+        cp -a -rf ${./.}/examples/${example}/. "$WS_NAME"
+        chmod -R +w "$WS_NAME"
+        mv "$WS_NAME" "$out"
         mkdir "$out"/.idx
         cp ${./dev.nix} "$out"/.idx/dev.nix
         chmod -R u+w "$out"
