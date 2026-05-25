f6c4144ea817474a1f6d528f6e3cbd887356d045
Rody Davis <rody.davis.jr@gmail.com>
2026-05-24 22:39:05 -0700

    feat(preact_signals): implement SignalOptionsBase inheritance hierarchy for options classes

--- packages/preact_signals/lib/src/options.dart
+++ packages/preact_signals/lib/src/options.dart
@@ -1,26 +1,18 @@
-/// Configuration options for a [Signal].
-class SignalOptions<T> {
-  /// The name of the signal for debugging purposes.
+/// Base configuration options for reactive components and signals.
+abstract class SignalOptionsBase {
+  /// The name for debugging purposes.
   final String? name;
 
-  /// Callback called when the signal goes from 0 to >=1 listeners.
-  final void Function()? watched;
+  const SignalOptionsBase({this.name});
+}
 
-  /// Callback called when the signal goes from >=1 to 0 listeners.
-  final void Function()? unwatched;
-
-  const SignalOptions({
-    this.name,
-    this.watched,
-    this.unwatched,
-  });
+/// Configuration options for reactive [Effect]s.
+class EffectOptions extends SignalOptionsBase {
+  const EffectOptions({super.name});
 }
 
 /// Configuration options for a [ReadonlySignal].
-class ReadonlySignalOptions<T> {
-  /// The name of the signal for debugging purposes.
-  final String? name;
-
+class ReadonlySignalOptions<T> extends SignalOptionsBase {
   /// Callback called when the signal goes from 0 to >=1 listeners.
   final void Function()? watched;
 
@@ -28,36 +20,26 @@ class ReadonlySignalOptions<T> {
   final void Function()? unwatched;
 
   const ReadonlySignalOptions({
-    this.name,
+    super.name,
     this.watched,
     this.unwatched,
+  });
+}
+
+/// Configuration options for a [Signal].
+class SignalOptions<T> extends ReadonlySignalOptions<T> {
+  const SignalOptions({
+    super.name,
+    super.watched,
+    super.unwatched,
   });
 }
 
 /// Configuration options for a [Computed] signal.
-class ComputedOptions<T> {
-  /// The name of the signal for debugging purposes.
-  final String? name;
-
-  /// Callback called when the signal goes from 0 to >=1 listeners.
-  final void Function()? watched;
-
-  /// Callback called when the signal goes from >=1 to 0 listeners.
-  final void Function()? unwatched;
-
+class ComputedOptions<T> extends ReadonlySignalOptions<T> {
   const ComputedOptions({
-    this.name,
-    this.watched,
-    this.unwatched,
-  });
-}
-
-/// Configuration options for reactive [Effect]s.
-class EffectOptions {
-  /// The name of the effect for debugging purposes.
-  final String? name;
-
-  const EffectOptions({
-    this.name,
+    super.name,
+    super.watched,
+    super.unwatched,
   });
 }
