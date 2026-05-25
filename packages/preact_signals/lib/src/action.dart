70249967087e9c9c7aed0b8d3e800d435eb0c16c
Rody Davis <rody.davis.jr@gmail.com>
2026-05-25 01:47:00 -0700

    feat: implement idiomatic toAction, toActionList function extensions and actionList

--- packages/preact_signals/lib/src/action.dart
+++ packages/preact_signals/lib/src/action.dart
@@ -10,6 +10,8 @@ class _Sentinel {
 ///
 /// When the returned function is called, it executes the original function
 /// inside a [batch] and [untracked] block.
+/// Supports up to 10 positional arguments. For functions with more arguments,
+/// use [actionList].
 Function action(Function fn, {ActionOptions? options}) {
   return ([
     Object? a0 = const _Sentinel(),
@@ -18,6 +20,10 @@ Function action(Function fn, {ActionOptions? options}) {
     Object? a3 = const _Sentinel(),
     Object? a4 = const _Sentinel(),
     Object? a5 = const _Sentinel(),
+    Object? a6 = const _Sentinel(),
+    Object? a7 = const _Sentinel(),
+    Object? a8 = const _Sentinel(),
+    Object? a9 = const _Sentinel(),
   ]) {
     return batch(() {
       return untracked(() {
@@ -28,9 +34,63 @@ Function action(Function fn, {ActionOptions? options}) {
         if (a3 is! _Sentinel) args.add(a3);
         if (a4 is! _Sentinel) args.add(a4);
         if (a5 is! _Sentinel) args.add(a5);
+        if (a6 is! _Sentinel) args.add(a6);
+        if (a7 is! _Sentinel) args.add(a7);
+        if (a8 is! _Sentinel) args.add(a8);
+        if (a9 is! _Sentinel) args.add(a9);
         return Function.apply(fn, args);
       });
     });
   };
 }
 
+/// Wraps a callback function in an action that is invoked with a list of arguments.
+///
+/// This has no limit on the number of arguments.
+Function actionList(Function fn, {ActionOptions? options}) {
+  return (List<dynamic> args) {
+    return batch(() {
+      return untracked(() {
+        return Function.apply(fn, args);
+      });
+    });
+  };
+}
+
+/// Extension on 0-argument functions to wrap them in a type-safe action.
+extension ActionFunction0Extension<R> on R Function() {
+  /// Wraps this function in a type-safe action.
+  R Function() toAction({ActionOptions? options}) {
+    return () => batch(() => untracked(this));
+  }
+}
+
+/// Extension on 1-argument functions to wrap them in a type-safe action.
+extension ActionFunction1Extension<A, R> on R Function(A) {
+  /// Wraps this function in a type-safe action.
+  R Function(A) toAction({ActionOptions? options}) {
+    return (a) => batch(() => untracked(() => this(a)));
+  }
+}
+
+/// Extension on 2-argument functions to wrap them in a type-safe action.
+extension ActionFunction2Extension<A, B, R> on R Function(A, B) {
+  /// Wraps this function in a type-safe action.
+  R Function(A, B) toAction({ActionOptions? options}) {
+    return (a, b) => batch(() => untracked(() => this(a, b)));
+  }
+}
+
+/// Extension on any function to wrap it in an action that accepts a list of arguments.
+extension ActionFunctionListExtension on Function {
+  /// Wraps this function in an action that can be invoked with a list of arguments.
+  dynamic Function(List<dynamic> args) toActionList({ActionOptions? options}) {
+    return (List<dynamic> args) {
+      return batch(() {
+        return untracked(() {
+          return Function.apply(this, args);
+        });
+      });
+    };
+  }
+}
--- packages/preact_signals/test/action_test.dart
+++ packages/preact_signals/test/action_test.dart
@@ -32,6 +32,76 @@ void main() {
       myAction('opt');
       expect(a.value, 'opt');
     });
+
+    test('should support up to 10 positional arguments', () {
+      final a = signal(0);
+      final myAction = action((int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10) {
+        a.value = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10;
+      });
+
+      myAction(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+      expect(a.value, 55);
+    });
+
+    test('actionList should support unlimited arguments', () {
+      final a = signal(0);
+      final myAction = actionList((int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11) {
+        a.value = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 + a11;
+      });
+
+      myAction([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+      expect(a.value, 66);
+    });
+
+    test('toAction extensions should be type-safe and run in batch/untracked', () {
+      final a = signal('a');
+      final b = signal('b');
+      final spy = Spy(() => '${a.value} ${b.value}');
+      effect(spy.call);
+      spy.resetHistory();
+
+      // 0-argument extension
+      final myAction0 = (() {
+        a.value = 'aa';
+        b.value = 'bb';
+      }).toAction();
+
+      myAction0();
+      expect(spy.calls, 1);
+      expect(a.value, 'aa');
+      expect(b.value, 'bb');
+
+      // 1-argument extension
+      final myAction1 = ((String val) {
+        a.value = val;
+        b.value = val;
+      }).toAction();
+
+      myAction1('xx');
+      expect(spy.calls, 2);
+      expect(a.value, 'xx');
+      expect(b.value, 'xx');
+
+      // 2-argument extension
+      final myAction2 = ((String nextA, String nextB) {
+        a.value = nextA;
+        b.value = nextB;
+      }).toAction();
+
+      myAction2('yy', 'zz');
+      expect(spy.calls, 3);
+      expect(a.value, 'yy');
+      expect(b.value, 'zz');
+    });
+
+    test('toActionList extension should support unlimited list-based arguments', () {
+      final a = signal(0);
+      final myAction = ((int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11) {
+        a.value = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 + a11;
+      }).toActionList();
+
+      myAction([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+      expect(a.value, 66);
+    });
   });
 }
-
